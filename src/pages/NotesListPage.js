5b4ceebb08fdd01c634f653005500893bf653f79
Artur Kosmatka <artur.kosmatka@gmail.com>
2021-11-11 19:27:58 +0000

    cors problem

--- src/pages/NotesListPage.js
+++ src/pages/NotesListPage.js
@@ -11,7 +11,7 @@ const NotesListPage = () => {
     }, []);
 
     const getNotes = async() => {
-     const response = await fetch('http://127.0.0.1:8000/api/notes/')
+     const response = await fetch('https://127.0.0.1:8000/api/notes/')
      const data = await response.json();
      setNotes(data);
     }
