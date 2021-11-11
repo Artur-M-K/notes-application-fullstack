276452437032d04e6fe48118e5b47f306b671953
Artur Kosmatka <artur.kosmatka@gmail.com>
2021-11-11 20:59:16 +0000

    cors problem

--- notes/settings.py
+++ notes/settings.py
@@ -147,3 +147,12 @@ CORS_ALLOW_ALL_ORIGINS = True
 #         'https://127.0.0.1:8000',
     
 #     )
+
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
