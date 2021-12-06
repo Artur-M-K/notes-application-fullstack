0b107772029f88dfd42f279e6427860d1196ce95
Artur Kosmatka <artur.kosmatka@gmail.com>
2021-12-06 22:33:46 +0000

    for deploy ver 3

--- notes/settings.py
+++ notes/settings.py
@@ -27,7 +27,7 @@ if os.path.isfile(dotenv_file):
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['notes-view.herokuapp.com', '127.0.0.1', 'localhost']
 
